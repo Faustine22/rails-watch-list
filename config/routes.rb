fb7ca0499a77314486afef8c947216658834536a
faustine pena <marlau.p@hotmail.fr>
2023-02-24 16:28:45 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-    resources :lists, only: %i[create new show index] do
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
       resources :bookmarks, only: %i[new, create]
     end
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     resources :bookmarks, only: %i[destroy]
     # Defines the root path route ("/")
     # root "articles#index"
-  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
